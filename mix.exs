f29b5abb1f12a93629da715e83d394ddd166a669
Matt Oakley <matt.oakley@hashbanghq.com>
2017-07-26 13:39:16 +0100

    add packages to generate documentation

--- mix.exs
+++ mix.exs
@@ -5,6 +5,8 @@ defmodule Issues.Mixfile do
     [app: :issues,
      escript: escript_config(),
      version: "0.1.0",
+     name: "Issues",
+     source_url: "https://github.com/matoakley/elixir-gh-issue-tracker",
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
@@ -30,8 +32,10 @@ defmodule Issues.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     [
-      httpoison: "~> 0.9",
-      poison: "~> 2.2"
+      { :earmark, "~> 1.0", override: true },
+      { :ex_doc, "~> 0.12" },
+      { :httpoison, "~> 0.9" },
+      { :poison, "~> 2.2" }
     ]
   end
 
