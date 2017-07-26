ec0616fe7643ac04a2e4f53de1a933ee4a9d5047
Matt Oakley <matt.oakley@hashbanghq.com>
2017-07-26 09:32:24 +0100

    move github api url to config

--- lib/issues/github_issues.ex
+++ lib/issues/github_issues.ex
@@ -1,4 +1,5 @@
 defmodule Issues.GithubIssues do
+  @github_url Application.get_env(:issues, :github_url)
   @user_agent [ {"User-agent", "Elixir m@matthewoakley.co.uk"} ]
 
   def fetch(user, project) do
@@ -8,7 +9,7 @@ defmodule Issues.GithubIssues do
   end
 
   def issues_url(user, project) do
-    "https://api.github.com/repos/#{user}/#{project}/issues"
+    "#{@github_url}/repos/#{user}/#{project}/issues"
   end
 
   def handle_response({:ok, %{status_code: 200, body: body}}) do
